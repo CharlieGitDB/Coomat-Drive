7cddffb9af9d617e6c8fe41b1b97104907b1b6b6
Charlie <charliedevelopmentaccess@gmail.com>
2019-01-15 19:43:13 -0600

    fix front end formatting

--- client/src/App.js
+++ client/src/App.js
@@ -8,36 +8,36 @@ import AuthService from './providers/AuthService'
 
 import './App.css'
 
-class App extends Component {  
-  componentDidMount() {
-    this.watchAuth()
-  }
- 
-  state = {
-    hideLoader: false,
-    hasAuth: false
-  }
+class App extends Component {
+    componentDidMount() {
+        this.watchAuth()
+    }
 
-  watchAuth() {
-    AuthService.getAuth().then(authStatus => {
-      AuthService.onAuthStateChange().subscribe(hasAuth => {
-        this.setState({
-          hideLoader: true,
-          hasAuth: hasAuth
+    state = {
+        hideLoader: false,
+        hasAuth: false
+    }
+
+    watchAuth() {
+        AuthService.getAuth().then(authStatus => {
+            AuthService.onAuthStateChange().subscribe(hasAuth => {
+                this.setState({
+                    hideLoader: true,
+                    hasAuth: hasAuth
+                })
+            })
         })
-      })
-    })
-  }
+    }
 
-  render() {
-    return (
-      <div className="App">
-        { this.state.hideLoader ? null : <FullLoader /> }
-        <Header hasAuth={this.state.hasAuth} />
-        <Main hasAuth={this.state.hasAuth} />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                {this.state.hideLoader ? null : <FullLoader />}
+                <Header hasAuth={this.state.hasAuth} />
+                <Main hasAuth={this.state.hasAuth} />
+            </div>
+        )
+    }
 }
 
 export default App
