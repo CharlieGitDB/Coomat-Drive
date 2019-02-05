b572b7c09a8edb9b76d5dcfbd8f032a63ac3fcf8
Charlie <charliedevelopmentaccess@gmail.com>
2019-02-05 07:19:15 -0600

    Add error log for when mysql isnt running

--- backend/providers/Sql.js
+++ backend/providers/Sql.js
@@ -3,7 +3,10 @@ const STATIC_VARS = require('../models/StaticVariables')
 
 const sqlize = new Sequelize(STATIC_VARS.dburl)
 
-sqlize.query('CREATE DATABASE IF NOT EXISTS coomatdrive;').catch(err => process.exit(0))
+sqlize.query('CREATE DATABASE IF NOT EXISTS coomatdrive;').catch(err => {
+    console.error('MySql server is most likely not started, exiting..')
+    process.exit(0)
+})
 
 //add sequelize to the new object to use the object type variables
 sqlize.seq = Sequelize
